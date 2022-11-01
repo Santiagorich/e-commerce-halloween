ba4a63c47cfef97ef8d9abb5b70a41764a8b12bf
ValRab <Santiagorecoba@hotmail.es>
2022-11-01 07:01:45 -0300

    Cart done

--- src/components/ProductGrid/ProductGrid.jsx
+++ src/components/ProductGrid/ProductGrid.jsx
@@ -1,57 +1,71 @@
-import React from 'react'
-import ProductCard from '../ProductCard'
+import ProductCard from "../ProductCard";
 
-function ProductGrid () {
+function ProductGrid({ setCartProducts, setCartToggled,cartProducts }) {
   const products = [
     {
       id: 0,
-      img: 'assets/trick-treat1-img.png',
+      name: "Toffee",
+      img: "assets/trick-treat1-img.png",
       price: 10,
-      description: 'Description here'
+      description: "Description here",
     },
     {
       id: 1,
-      img: 'assets/trick-treat2-img.png',
+      name: "Toffee",
+      img: "assets/trick-treat2-img.png",
       price: 10,
-      description: 'Description here'
+      description: "Description here",
     },
     {
       id: 2,
-      img: 'assets/trick-treat3-img.png',
+      name: "Toffee",
+      img: "assets/trick-treat3-img.png",
       price: 10,
-      description: 'Description here'
+      description: "Description here",
     },
     {
       id: 3,
-      img: 'assets/trick-treat4-img.png',
+      name: "Toffee",
+      img: "assets/trick-treat4-img.png",
       price: 10,
-      description: 'Description here'
+      description: "Description here",
     },
     {
       id: 4,
-      img: 'assets/trick-treat5-img.png',
+      name: "Toffee",
+      img: "assets/trick-treat5-img.png",
       price: 10,
-      description: 'Description here'
+      description: "Description here",
     },
     {
       id: 5,
-      img: 'assets/trick-treat6-img.png',
+      name: "Toffee",
+      img: "assets/trick-treat6-img.png",
       price: 10,
-      description: 'Aguacate'
-    }
-  ]
+      description: "Aguacate",
+    },
+  ];
   return (
-    <div className='flex w-full flex-col justify-center'>
-      <span className='w-full text-center text-4xl font-semibold'>
+    <div className="flex w-full flex-col justify-center">
+      <span className="w-full text-center text-4xl font-semibold">
         Truco o trato
       </span>
-      <div className='mx-auto grid w-fit grid-cols-3 justify-center gap-x-16 gap-y-16 p-24'>
-        {products.map((product) => (
-          <ProductCard key={products.id} {...product} />
-        ))}
+      <div className="mx-auto grid w-fit grid-cols-3 justify-center gap-x-16 gap-y-16 p-24">
+        {products.map((product, index) => {
+          console.log(product);
+          return (
+            <ProductCard
+              key={index}
+              product={product}
+              setCartProducts={setCartProducts}
+              setCartToggled={setCartToggled}
+              cartProducts={cartProducts}
+            />
+          );
+        })}
       </div>
     </div>
-  )
+  );
 }
 
-export default ProductGrid
+export default ProductGrid;
